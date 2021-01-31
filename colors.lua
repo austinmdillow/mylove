2cb11499e13f92c6d24d87a05059023349a12b4d
austinmdillow <austinmdillow@gmail.com>
2021-01-30 22:25:46 -0500

    making colors table read only

--- colors.lua
+++ colors.lua
@@ -6,3 +6,15 @@ COLORS = {
   blue = {0,0,1},
   periwinkle = {127/255, 0, 255/255}
 }
+
+function protectTable(tbl)
+    return setmetatable({}, {
+        __index = tbl,
+        __newindex = function(t, key, value)
+            error("attempting to change constant " ..
+                   tostring(key) .. " to " .. tostring(value), 2)
+        end
+    })
+end
+
+COLORS = protectTable(COLORS)
